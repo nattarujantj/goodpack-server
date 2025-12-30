1dce1a5e35bd175204e14ba45a8db890706e8442
Pukky <nattaruja.b@lmwn.com>
2025-12-30 16:18:14 +0700

    refactor: เอา branchName ออกจาก CustomerBankAccount

--- models/customer_bank_account.go
+++ models/customer_bank_account.go
@@ -5,7 +5,6 @@ type CustomerBankAccount struct {
 	BankName      string `bson:"bankName" json:"bankName"`           // ชื่อธนาคาร
 	AccountName   string `bson:"accountName" json:"accountName"`     // ชื่อบัญชี
 	AccountNumber string `bson:"accountNumber" json:"accountNumber"` // เลขบัญชี
-	BranchName    string `bson:"branchName" json:"branchName"`       // สาขา
 	IsDefault     bool   `bson:"isDefault" json:"isDefault"`         // บัญชีหลัก
 }
 
